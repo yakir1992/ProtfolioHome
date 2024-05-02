d46c107f9d74ed2dc5a5af62ceb6b86135555f0f
yakir1992 <yakir1992@gmail.com>
2024-05-02 16:33:36 +0300

    netflix changes

--- pages/works/netflix.js
+++ pages/works/netflix.js
@@ -23,7 +23,7 @@ const Work = () => (
       <List ml={4} my={4}>
         <ListItem>
           <Meta>Platform</Meta>
-          <span>Windows/macOS/Linux/iOS/Android</span>
+          <span>Windows/macOS/Linux</span>
         </ListItem>
         <ListItem>
           <Meta>Stack</Meta>
