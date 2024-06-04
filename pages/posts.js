7817eded40c46cd5e4e678ae01dbe65d5a4cc48c
yakir1992 <yakir1992@gmail.com>
2024-06-04 13:13:04 +0300

    posts updated

--- pages/posts.js
+++ pages/posts.js
@@ -3,7 +3,7 @@ import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { GridItem } from '../components/grid-item'
 
-import thumbPortfolio from '../public/images/contents/youtube-how-to-build-portfolio.jpg'
+import ansibleAutomation from '../public/images/contents/ansible.png'
 import thumbHowToUseInkdrop from '../public/images/contents/youtube-how-to-use-inkdrop.jpg'
 
 const Posts = () => (
@@ -13,20 +13,20 @@ const Posts = () => (
         Popular Posts
       </Heading>
 
-      {/* <Section delay={0.1}>
+      { <Section delay={0.1}>
         <SimpleGrid columns={[1, 2, 2]} gap={6}>
           <GridItem
-            title="How to build a portfolio website"
-            thumbnail={thumbPortfolio}
-            href="https://www.youtube.com/watch?v=bSMZgXzC9AA"
+            title="Ansible: The Power of IT Automation"
+            thumbnail={ansibleAutomation}
+            href="https://medium.com/@yakir1992/exploring-ansible-the-power-of-it-automation-2235ab4bf017"
           />
-          <GridItem
+          {/* <GridItem
             title="How to take notes in Markdown efficiently with Inkdrop"
             thumbnail={thumbHowToUseInkdrop}
             href="https://www.youtube.com/watch?v=-qBavwqc_mY"
-          />
+          /> */}
         </SimpleGrid>
-      </Section> */}
+      </Section>}
     </Container>
   </Layout>
 )
