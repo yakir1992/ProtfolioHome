3c5c74649c4db89dda1141e8bc437f26639134b8
yakir1992 <yakir1992@gmail.com>
2025-03-08 06:14:10 +0200

    dark theme

--- components/chakra.js
+++ components/chakra.js
@@ -1,17 +1,10 @@
-import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import theme from '../lib/theme'
 
 export function Chakra({ cookies, children }) {
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeProvider
-        options={{
-          initialColorMode: 'light',
-          useSystemColorMode: true
-        }}
-      >
-        {children}
-      </ColorModeProvider>
+      {children}
     </ChakraProvider>
   )
 }
